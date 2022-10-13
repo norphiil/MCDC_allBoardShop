8b73a29be366f5aa5273165e33299f0d6e8ad576
Florent CADET <norphiil@hotmail.fr>
2022-10-13 16:07:43 +0200

    make Information_personnel constructor

--- src/main/java/net/TP1/class/Information_personnel.java
+++ src/main/java/net/TP1/class/Information_personnel.java
@@ -1,7 +1,19 @@
-    class Information_personnel {
-        String nom;
-        String prenom;
-        String adresse;
-        String mail;
-        String tel;
+class Information_personnel {
+    String nom;
+    String prenom;
+    String adresse;
+    String mail;
+    String tel;
+
+    Information_personnel(String nom, String prenom, String adresse, String mail){
+        this(nom, prenom, adresse, mail, null)
     }
+
+    Information_personnel(String nom, String prenom, String adresse, String mail, String tel){
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+        this.mail = mail;
+        this.tel = tel;
+    }
+}
