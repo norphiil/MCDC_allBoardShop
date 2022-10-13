d517b02768fb0ab4cc3dc6f2238ed76e37e4e11f
Florent CADET <norphiil@hotmail.fr>
2022-10-13 16:07:30 +0200

    make Fournisseur constructor

--- src/main/java/net/TP1/class/Fournisseur.java
+++ src/main/java/net/TP1/class/Fournisseur.java
@@ -2,4 +2,20 @@
         Activite activite;
         ArrayList<Vente> ventes = new ArrayList<>();
         Materiel materiel;
+
+        Fournisseur(String nom, String prenom, String adresse, String mail, Materiel materiel, Activite activite){
+            this(nom, prenom, adresse, mail, materiel, activite, null, null);
+        }
+        Fournisseur(String nom, String prenom, String adresse, String mail, Materiel materiel, Activite activite, String tel){
+            this(nom, prenom, adresse, mail, materiel, activite, tel, null);
+        }
+        Fournisseur(String nom, String prenom, String adresse, String mail, Materiel materiel, Activite activite, ArrayList<Vente> ventes){
+            this(nom, prenom, adresse, mail, materiel, activite, null, ventes);
+        }
+        Fournisseur(String nom, String prenom, String adresse, String mail, Materiel materiel, Activite activite, String tel, ArrayList<Vente> ventes){
+            this.super(nom, prenom, adresse, mail, tel);
+            this.activite = activite;
+            this.ventes = ventes;
+            this.materiel = materiel;
+        }
     }
