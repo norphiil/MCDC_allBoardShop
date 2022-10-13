61fba1afcfaf3817f13d9cf315d2f81e33bced18
Florent CADET <norphiil@hotmail.fr>
2022-10-13 16:12:31 +0200

    make Vente constructor

--- src/main/java/net/TP1/class/Vente.java
+++ src/main/java/net/TP1/class/Vente.java
@@ -1,8 +1,17 @@
-    class Vente {
-        String nom;
-        DateTime date_debut;
-        DateTime date_fin;
-        String description;
-        ArrayList<Activite> activites;
-        Vente_Produit vente_produit;
+class Vente {
+    String nom;
+    DateTime date_debut;
+    DateTime date_fin;
+    String description;
+    ArrayList<Activite> activites;
+    Vente_Produit vente_produit;
+
+    Vente(String nom, DateTime date_debut, DateTime date_fin, String description, Vente_Produit vente_produit, ArrayList<Activite> activites) {
+        this.nom = nom;
+        this.date_debut = date_debut;
+        this.date_fin = date_fin;
+        this.description = description;
+        this.activites = activites;
+        this.Vente_Produit = vente_produit;
     }
+}
