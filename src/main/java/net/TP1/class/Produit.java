d4d8c46b6efaffff170a3c6d011bc95a1851f3a2
Florent CADET <norphiil@hotmail.fr>
2022-10-13 16:10:12 +0200

    make Produit constructor

--- src/main/java/net/TP1/class/Produit.java
+++ src/main/java/net/TP1/class/Produit.java
@@ -1,7 +1,15 @@
-    class Produit {
-        String nom;
-        String description;
-        String photo;
-        Float prix_base;
-        Float prix_vente;
+class Produit {
+    String nom;
+    String description;
+    String photo;
+    Float prix_base;
+    Float prix_vente;
+
+    Produit(String nom, String description, String photo, Float prix_base, Float prix_vente){
+        this.nom = nom;
+        this.description = description;
+        this.photo = photo;
+        this.prix_base = prix_base;
+        this.prix_vente = prix_vente;
     }
+}
