7cb128e805ef2d8cf9f046b424a3cf379ab3b08a
Florent CADET <norphiil@hotmail.fr>
2022-10-13 16:00:41 +0200

    make Client constructor

--- src/main/java/net/TP1/class/Client.java
+++ src/main/java/net/TP1/class/Client.java
@@ -1,4 +1,19 @@
-    class Client extends Information_personnel {
-        Bool privilegie = false;
-        ArrayList<Activite> activites = new ArrayList<>();
+class Client extends Information_personnel {
+    Bool privilegie = false;
+    ArrayList<Activite> activites;
+
+    Client(String nom, String prenom, String adresse, String mail, ArrayList<Activite> activites){
+        this(nom, prenom, adresse, mail, null, false, activites);
     }
+    Client(String nom, String prenom, String adresse, String mail, ArrayList<Activite> activites, String tel){
+        this(nom, prenom, adresse, mail, tel, false, activites);
+    }
+    Client(String nom, String prenom, String adresse, String mail, ArrayList<Activite> activites, Bool privilegie){
+        this(nom, prenom, adresse, mail, null, privilegie, activites);
+    }
+    Client(String nom, String prenom, String adresse, String mail, ArrayList<Activite> activites, String tel, Bool privilegie){
+        this.super(nom, prenom, adresse, mail, tel);
+        this.privilegie = privilegie;
+        this.activites = activites;
+    }
+}
