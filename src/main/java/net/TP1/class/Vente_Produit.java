f0501d9b41e307c75bcff088d6eb975c1435728a
Florent CADET <norphiil@hotmail.fr>
2022-10-13 16:13:22 +0200

    make Vente_Produit constructor

--- src/main/java/net/TP1/class/Vente_Produit.java
+++ src/main/java/net/TP1/class/Vente_Produit.java
@@ -1,5 +1,11 @@
-    class Vente_Produit {
-        Integer quantite;
-        Taille taille;
-        ArrayList<Produits> produits = new ArrayList<>();
+class Vente_Produit {
+    Integer quantite;
+    Taille taille;
+    ArrayList<Produits> produits = new ArrayList<>();
+
+    Vente_Produit(Integer quantite, Taille taille, ArrayList<Produits> produits){
+        this.quantite = quantite;
+        this.taille = taille;
+        this.produits = produits;
     }
+}
