41b90ea32b302fe93b053664909ad7d325f835ec
Florent CADET <57914227+norphiil@users.noreply.github.com>
2022-10-13 20:21:10 +0200

    change forgot Information_personnel to Information_personnelle

--- src/main/java/net/TP1/class/Information_personnelle.java
+++ src/main/java/net/TP1/class/Information_personnelle.java
@@ -1,15 +1,15 @@
-class Informations_personnelles {
+class Information_personnelle {
     String nom;
     String prenom;
     String adresse;
     String mail;
     String tel;
 
-    Information_personnel(String nom, String prenom, String adresse, String mail){
+    Information_personnelle(String nom, String prenom, String adresse, String mail){
         this(nom, prenom, adresse, mail, null);
     }
 
-    Information_personnel(String nom, String prenom, String adresse, String mail, String tel){
+    Information_personnelle(String nom, String prenom, String adresse, String mail, String tel){
         this.nom = nom;
         this.prenom = prenom;
         this.adresse = adresse;
